cc16a7eddc466159553c1a967f225842e0f18cce
Michael-Maina <michaelmaina02@gmail.com>
2023-01-24 19:52:56 +0300

    Added 1-insertion_sort_list.c and 1-O

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers using
+ *                       the insertion sort algorithm
+ *
+ * @list: doubly linked list of integers to be sorted
+ * Return: nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = *list, *temp;
+
+	if (current == NULL)
+		return;
+
+	while (current)
+	{
+		temp = current;
+
+		while (temp->prev && temp->prev->n > temp->n)
+		{
+			temp->prev->next = temp->next;
+
+			if (temp->next)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			if (!temp->prev)
+				*list = temp;
+			else
+				temp->prev->next = temp;
+
+			print_list(*list);
+
+		}
+		current = current->next;
+	}
+}
